2e81abb9d3a6f52cd0b83b6b365980088031d7b5
Nehasri H <nehasri2210050@ssn.edu.in>
2025-07-18 21:02:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-let express = require("express");
-let app = express();
-let httpServer = require("http").createServer(app);
-let io = require("socket.io")(httpServer, {
+const express = require("express");
+const app = express();
+const httpServer = require("http").createServer(app);
+const io = require("socket.io")(httpServer, {
   cors: {
     origin: "*",
     methods: ["GET", "POST"]
@@ -14,22 +14,38 @@ io.on("connect", (socket) => {
   connections.push(socket);
   console.log(`${socket.id} has connected`);
 
+  // WebRTC signaling
   socket.on("propogate", (data) => {
-    connections.map((con) => {
+    connections.forEach((con) => {
       if (con.id !== socket.id) {
         con.emit("onpropogate", data);
       }
     });
   });
 
+  // 🎨 Real-time whiteboard events
+  socket.on("draw", (data) => {
+    socket.broadcast.emit("ondraw", data);
+  });
+
+  socket.on("mousedown", (data) => {
+    socket.broadcast.emit("onmousedown", data);
+  });
+
+  socket.on("clear", () => {
+    socket.broadcast.emit("onclear");
+  });
+
+  // Handle disconnect
   socket.on("disconnect", () => {
     console.log(`${socket.id} is disconnected`);
     connections = connections.filter((con) => con.id !== socket.id);
   });
 });
 
+// Serve frontend
 app.use(express.static("public"));
 
-let PORT = process.env.PORT || 8080;
+// Start server
+const PORT = process.env.PORT || 8080;
 httpServer.listen(PORT, () => console.log(`Server started on port ${PORT}`));
-
